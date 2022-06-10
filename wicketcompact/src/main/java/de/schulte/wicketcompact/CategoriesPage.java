d9d944e24cb3fde8d3c9b19083dbf2cfdc20364b
Christian Krauss <christian.krauss@exedio.com>
2022-06-10 14:34:27 +0200

    Show table of categories and images

--- wicketcompact/src/main/java/de/schulte/wicketcompact/CategoriesPage.java
+++ wicketcompact/src/main/java/de/schulte/wicketcompact/CategoriesPage.java
@@ -1,11 +1,18 @@
 package de.schulte.wicketcompact;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.wicket.AttributeModifier;
+import org.apache.wicket.markup.html.WebComponent;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.list.ListItem;
+import org.apache.wicket.markup.html.list.ListView;
+import org.apache.wicket.request.mapper.parameter.PageParameters;
+
 import de.schulte.wicketcompact.entities.Category;
 import de.schulte.wicketcompact.services.CategoryService;
 import de.schulte.wicketcompact.services.ServiceRegistry;
-import org.apache.wicket.markup.html.basic.Label;
-import org.apache.wicket.markup.repeater.RepeatingView;
-import org.apache.wicket.request.mapper.parameter.PageParameters;
 
 public class CategoriesPage extends BaseEntitiesPage {
 
@@ -19,10 +26,17 @@ public class CategoriesPage extends BaseEntitiesPage {
     @Override
     protected void onInitialize() {
         super.onInitialize();
-        final RepeatingView categories = new RepeatingView("categories");
-        for (Category category : categoryService.listAll()) {
-            categories.add(new Label(categories.newChildId(), category.getName()));
-        }
-        add(categories);
+        
+        final List<Category> categories = new ArrayList<>(categoryService.listAll());
+        final ListView<Category> categoryList = new ListView<Category>("categories", categories) {
+            @Override
+            protected void populateItem(final ListItem<Category> item) {
+                item.add(new Label("name", item.getModelObject().getName()));
+                final WebComponent image = new WebComponent("image");
+                image.add(new AttributeModifier("src", item.getModelObject().getImageUrl()));
+                item.add(image);
+            }
+        };
+        add(categoryList);
     }
 }
