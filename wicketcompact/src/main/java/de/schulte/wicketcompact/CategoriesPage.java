d3d8d20c952512c3cc27f598e64773307b8f25c4
Christian Krauss <christian.krauss@exedio.com>
2022-06-10 15:05:27 +0200

    Show pageable table of categories

--- wicketcompact/src/main/java/de/schulte/wicketcompact/CategoriesPage.java
+++ wicketcompact/src/main/java/de/schulte/wicketcompact/CategoriesPage.java
@@ -6,8 +6,11 @@ import java.util.List;
 import org.apache.wicket.AttributeModifier;
 import org.apache.wicket.markup.html.WebComponent;
 import org.apache.wicket.markup.html.basic.Label;
-import org.apache.wicket.markup.html.list.ListItem;
-import org.apache.wicket.markup.html.list.ListView;
+import org.apache.wicket.markup.html.navigation.paging.PagingNavigator;
+import org.apache.wicket.markup.repeater.Item;
+import org.apache.wicket.markup.repeater.data.DataView;
+import org.apache.wicket.markup.repeater.data.IDataProvider;
+import org.apache.wicket.markup.repeater.data.ListDataProvider;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 
 import de.schulte.wicketcompact.entities.Category;
@@ -16,27 +19,28 @@ import de.schulte.wicketcompact.services.ServiceRegistry;
 
 public class CategoriesPage extends BaseEntitiesPage {
 
-    private final CategoryService categoryService;
-
-    public CategoriesPage(PageParameters parameters) {
+    public CategoriesPage(final PageParameters parameters) {
         super(parameters);
-        categoryService = ServiceRegistry.get(CategoryService.class);
     }
 
     @Override
     protected void onInitialize() {
         super.onInitialize();
         
+        final CategoryService categoryService = ServiceRegistry.get(CategoryService.class);
         final List<Category> categories = new ArrayList<>(categoryService.listAll());
-        final ListView<Category> categoryList = new ListView<Category>("categories", categories) {
+        final IDataProvider<Category> categoriesProvider = new ListDataProvider<>(categories);
+        final DataView<Category> categoriesView = new DataView<Category>("categories", categoriesProvider) {
             @Override
-            protected void populateItem(final ListItem<Category> item) {
+            protected void populateItem(final Item<Category> item) {
                 item.add(new Label("name", item.getModelObject().getName()));
                 final WebComponent image = new WebComponent("image");
                 image.add(new AttributeModifier("src", item.getModelObject().getImageUrl()));
                 item.add(image);
             }
         };
-        add(categoryList);
+        categoriesView.setItemsPerPage(3);
+        add(categoriesView);
+        add(new PagingNavigator("navigator", categoriesView));
     }
 }
