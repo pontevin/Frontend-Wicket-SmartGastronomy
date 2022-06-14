afb277cc108c2b4527606012595a798e3c1a0083
Christian Krauss <christian.krauss.76@googlemail.com>
2022-06-14 21:49:47 +0200

    Add EntitiesView component to show table of entities

--- wicketcompact/src/main/java/de/schulte/wicketcompact/CategoriesPage.java
+++ wicketcompact/src/main/java/de/schulte/wicketcompact/CategoriesPage.java
@@ -1,16 +1,10 @@
 package de.schulte.wicketcompact;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.apache.wicket.AttributeModifier;
+import org.apache.wicket.Component;
 import org.apache.wicket.markup.html.WebComponent;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.navigation.paging.PagingNavigation;
-import org.apache.wicket.markup.repeater.Item;
-import org.apache.wicket.markup.repeater.data.DataView;
-import org.apache.wicket.markup.repeater.data.IDataProvider;
-import org.apache.wicket.markup.repeater.data.ListDataProvider;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 
 import de.schulte.wicketcompact.entities.Category;
@@ -28,18 +22,17 @@ public class CategoriesPage extends BaseEntitiesPage {
         super.onInitialize();
         
         final CategoryService categoryService = ServiceRegistry.get(CategoryService.class);
-        final List<Category> categories = new ArrayList<>(categoryService.listAll());
-        final IDataProvider<Category> categoriesProvider = new ListDataProvider<>(categories);
-        final DataView<Category> categoriesView = new DataView<Category>("categories", categoriesProvider) {
+        final EntitiesView<Category> categoriesView = new EntitiesView<Category>("categories", categoryService.listAll(), 2) {
             @Override
-            protected void populateItem(final Item<Category> item) {
-                item.add(new Label("name", item.getModelObject().getName()));
+            protected Component[] getComponentsForEntity(final Category category) {
                 final WebComponent image = new WebComponent("image");
-                image.add(new AttributeModifier("src", item.getModelObject().getImageUrl()));
-                item.add(image);
+                image.add(new AttributeModifier("src", category.getImageUrl()));
+                return new Component[] {
+                    new Label("name", category.getName()),
+                    image
+                };
             }
         };
-        categoriesView.setItemsPerPage(3);
         add(categoriesView);
         add(new PagingNavigation("navigator", categoriesView));
     }
